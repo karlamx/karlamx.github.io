f11b1335cc342eaf91105a9f1c6cb6ce40d6bd49
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-09-06 19:38:09 -0400

    fixing errors

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 
-function functionmiddleeast(id)
+function functionstart(id)
 {
  	document.getElementById("smallscreen").style.backgroundImage = "url('broom.png')";
 document.getElementById("buttonmakeup").style.display = "block";
